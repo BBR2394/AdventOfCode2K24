92f81c793ba435b55fd8a709a126d688066a2b50
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2024-12-01 23:56:49 +0100

    ::sparkles: day 01 step 01 done :-)

--- day_01/main.py
+++ day_01/main.py
@@ -12,16 +12,48 @@ def readFile(av):
     lst = input.split('\n')
 
     lst_one = []
+    lst_one_int = []
+
     lst_two = []
+    lst_two_int = []
     for i in lst:
         temp = i.split(' ')
-        print(temp)
+        
         lst_one.append(temp[0]) 
         lst_two.append(temp[3]) 
 
-    print(lst_two)
-    fd.close()
+    for j in lst_one:
+        lst_one_int.append(int(j))
+    for j in lst_two:
+        lst_two_int.append(int(j))
+    print(lst_one_int)
 
+    lst_one_int.sort()
+    lst_two_int.sort()
+    print(lst_two_int)
+    fd.close()
+    x = 0
+    res = []
+    print("lst_one_int[x] : ", lst_one_int[x])
+    while x < len(lst_one_int):
+        tmp = 0
+        print("lst_one_int[x] : ", lst_one_int[x])
+        print("lst_two_int[x] : ", lst_two_int[x])
+
+        if (lst_one_int[x] > lst_two_int[x]):
+            tmp = lst_one_int[x] - lst_two_int[x]
+        elif (lst_one_int[x] < lst_two_int[x]):
+            tmp = lst_two_int[x] - lst_one_int[x]
+        else : 
+            print("erreur")
+        res.append(tmp)
+        print("A:", lst_one_int[x], " et ", lst_two_int[x], " et difference = ", tmp)
+        x += 1
+
+    finalRes = 0
+    for k in res:
+        finalRes += k
+    print("finalRes = ", finalRes)
 
 def main(av):
     print("Hello - day_01")
