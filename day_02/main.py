b2bd943d9bd7d1decc401ad980710dd8cb2e67d9
Baptiste BERTRAND-RAPELLO <baptiste.bertrand-rapello@dedalus.com>
2024-12-02 17:51:47 +0100

    :construction: step two in progress

--- day_02/main.py
+++ day_02/main.py
@@ -42,13 +42,44 @@ def check_order(line):
 
     # next = my_queue[0]
     # order(my_queue.popleft(), my_queue, None)
-    
+g_nf_last_faulty = 0
+def check_order_step_two(line):
+    my_queue = deque(line)
+    #print("check order : " , line)
+    order = None
+    cur = int(my_queue.popleft())
+    faulty = 0
+    while len(my_queue) > 0 :
+        #print(cur, " : ", my_queue[0])
+        if cur == int(my_queue[0]):
+            faulty += 1
+        if order == None:
+            order = cur < int(my_queue[0])
+        elif order == True:
+            #print("cur < int(my_queue[0])", cur < int(my_queue[0]))
+            if (cur < int(my_queue[0])) == False:
+                faulty += 1
+
+        elif order == False:
+            if (cur < int(my_queue[0])) == True:
+                faulty += 1
+            
+        #print("order = ", order)
+        cur = int(my_queue.popleft())
+    #print("on retrourne true")
+    #print("faulty = ", faulty)
+    g_nf_last_faulty = faulty
+    if faulty > 1 :
+        return False
+    else: 
+        return True
+
 def check_differ(line):
     my_queue = deque(line)
     cur = int(my_queue.popleft())
     while len(my_queue) > 0 :
         diff = abs(int(cur) - int(my_queue[0]))
-        if diff > 3 or diff < 1:
+        if diff > 3 :
             return False
         cur = int(my_queue.popleft())
         
@@ -58,7 +89,7 @@ def check_line(lst):
     finalRes = []
     for i in lst:
         line = i.split(' ')
-        check_one = check_order(line)
+        check_one = check_order_step_two(line)
         check_two = check_differ(line)
         #check_two = True
         #print("line : ", line, " check 1 : ", check_one, " check 2 : ", check_two)
