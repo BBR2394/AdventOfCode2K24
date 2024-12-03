40b051dcffd840c0d81876ce1ead11418f558fc4
Baptiste BERTRAND-RAPELLO <baptiste.bertrand-rapello@dedalus.com>
2024-12-03 15:14:33 +0100

    My script miss some multiplication to do, my state machine doesn't work well...

--- day_03/main.py
+++ day_03/main.py
@@ -3,6 +3,8 @@
 import sys
 from collections import deque
 
+import time
+
 my_queue = deque()
 res = 0
 
@@ -36,91 +38,95 @@ def check_mul(cur):
 
 def get_n_check_firstnum(cur, step, num): # step max is 3
     global my_queue 
-    print("get_n_check_firstnum cur ", cur, " step :  ", step, " num ", num)
+    #print("get_n_check_firstnum cur ", cur, " step :  ", step, " num ", num)
     if cur == "," and step == 1:
         return None
     elif cur != "," and step > 3:
         return None
     elif cur == ",":
-        print("ON RETOURNE NUM : ", num)
+        #print("ON RETOURNE NUM : ", num)
         return num  
 
     try :
         int(cur)
     except:
         return None
-    print("on detecte un nombre : ", int(cur))
+    #print("on detecte un nombre : ", int(cur))
     tmp = int(cur)
 
     if step == 1:
-        print("step : ", step, " tmp ", tmp)
+        #print("step : ", step, " tmp ", tmp)
         return get_n_check_firstnum(my_queue.popleft(), step+1, tmp)
     if step == 2 or step == 3:
-        print("step : ", step)
+        #print("step : ", step)
         return get_n_check_firstnum(my_queue.popleft(), step+1, num*10 + tmp)
 
 def get_n_check_secondNum(cur, step, num): # step max is 3
     global my_queue 
-    print("get_n_check_firstnum cur ", cur, " step :  ", step, " num ", num)
+    #print("get_n_check_firstnum cur ", cur, " step :  ", step, " num ", num)
     if cur == ")" and step == 1:
         return None
     elif cur != ")" and step > 3:
         return None
     elif cur == ")":
-        print("ON RETOURNE NUM : ", num)
+        #print("ON RETOURNE NUM : ", num)
         return num  
 
     try :
         int(cur)
     except:
         return None
-    print("on detecte un nombre : ", int(cur))
+    #print("on detecte un nombre : ", int(cur))
     tmp = int(cur)
 
     if step == 1:
-        print("step : ", step, " tmp ", tmp)
+        #print("step : ", step, " tmp ", tmp)
         return get_n_check_secondNum(my_queue.popleft(), step+1, tmp)
     if step == 2 or step == 3:
-        print("step : ", step)
+        #print("step : ", step)
         return get_n_check_secondNum(my_queue.popleft(), step+1, num*10 + tmp)
 
 
 def my_parser():
     global my_queue 
     global res
-    
+    counter_of_mul = 0
     first_num = 0
     second_num = 0
     while len(my_queue) > 0:
         cur = my_queue.popleft()
-        print("cursor au debut : ", cur, end=':')
+        print("cur:", cur, end=';')
         if cur == "m":
+            print("")
             if check_mul(cur):
-                print("il y a mul : cur : ", cur)
+                #print("il y a mul : cur : ", cur)
                 cur = my_queue.popleft()
                 if check_parenthesis(cur):
                     cur = my_queue.popleft()
-                    print("detection de parenthese ok : cur : ", cur)
+                    #print("detection de parenthese ok : cur : ", cur)
                     first_num = get_n_check_firstnum(cur, 1, first_num)
-                    print("first_num : ", first_num)
+                    #print("first_num : ", first_num)
                     cur = my_queue.popleft()
-                    print("cur : ", cur)
+                    #print("cur : ", cur)
                     if first_num != None:
                         second_num = get_n_check_secondNum(cur, 1, second_num)
-                        print("second_num : ", second_num)
+                        #print("second_num : ", second_num)
                         if second_num != None:
                             res += first_num*second_num
                             print("on arrete pour le moment, first_num :", first_num, "second num: ", second_num, "  res temp ", first_num*second_num, " Res final inter : ", res)
-                        else : 
-                            print("second num is None")
+                            counter_of_mul += 1
+                            #time.sleep(0.4)
+
                 else : 
                     print("pas de parenthese")
             print("cursor a la fin ", cur)
             #return True
             
+    print("nombre de mul : ", counter_of_mul)
     print("resultat final : ", res)
     return True
     
+# 14H : 168343063 answer is too low
 
 def lexer_parser(data):
     global my_queue 
